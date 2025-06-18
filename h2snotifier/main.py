d30f58e3c8a50e2d19c37af414488847dbb94d2d
sophiegranger289 <sophiegranger289@gmail.com>
2025-06-18 10:30:42 +0200

    Update main.py

--- h2snotifier/main.py
+++ h2snotifier/main.py
@@ -1,6 +1,4 @@
 import logging
-import time
-import schedule
 import json
 import os
 import gc
@@ -16,12 +14,15 @@ def read_config(config_path=os.path.join(os.path.dirname(__file__), "config.json
         return json.load(f)
 
 def is_within_run_window():
-    tz = pytz.timezone("Europe/Amsterdam")
+    tz = pytz.timezone("Europe/Amsterdam")  # 欧洲中部时间
     now = datetime.datetime.now(tz)
     current_time = now.time()
-    weekday = now.weekday()  # 0 = Monday, 6 = Sunday
+    weekday = now.weekday()  # Monday is 0, Sunday is 6
 
-    return weekday < 5 and datetime.time(8, 30) <= current_time <= datetime.time(17, 30)
+    is_weekday = weekday < 5
+    is_in_time_range = datetime.time(8, 30) <= current_time <= datetime.time(17, 30)
+
+    return is_weekday and is_in_time_range
 
 def scan_and_push(TELEGRAM_API_KEY, DEBUGGING_CHAT_ID):
     if not is_within_run_window():
@@ -37,12 +38,7 @@ def scan_and_push(TELEGRAM_API_KEY, DEBUGGING_CHAT_ID):
         chat_id = gp["chat_id"]
         telegram = TelegramBot(apikey=TELEGRAM_API_KEY, chat_id=chat_id)
 
-        houses_in_cities = scrape(
-            cities=cities,
-            apikey=TELEGRAM_API_KEY,
-            debug_chat_id=DEBUGGING_CHAT_ID
-        )
-
+        houses_in_cities = scrape(cities=cities, apikey=TELEGRAM_API_KEY, debug_chat_id=DEBUGGING_CHAT_ID)
         for city_id, houses in houses_in_cities.items():
             new_houses = sync_houses(city_id=city_id, houses=houses)
             for h in new_houses:
@@ -56,6 +52,7 @@ def scan_and_push(TELEGRAM_API_KEY, DEBUGGING_CHAT_ID):
                 except Exception as error:
                     debug_telegram.send_simple_msg(f"❌ 推送出错: {str(error)}")
                     debug_telegram.send_simple_msg(f"{h}")
+
     gc.collect()
 
 def main():
@@ -72,12 +69,4 @@ def main():
     )
 
     create_table()
-    scan_and_push(TELEGRAM_API_KEY, DEBUGGING_CHAT_ID)  # 先跑一次
-    schedule.every(3).seconds.do(lambda: scan_and_push(TELEGRAM_API_KEY, DEBUGGING_CHAT_ID))  # 每3秒一次
-
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
-
-if __name__ == "__main__":
-    main()
+    scan_and_push(TELEGRAM_API_KEY, DEBUGGING_CHAT_ID)
